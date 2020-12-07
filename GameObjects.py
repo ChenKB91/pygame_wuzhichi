422946f0aa5ece6e211b4e580d19679920da9d56
YuKai0928 <71370401+YuKai0928@users.noreply.github.com>
2020-12-07 14:38:58 +0800

    Add files via upload
    1207 defined Board

--- GameObjects.py
+++ GameObjects.py
@@ -7,19 +7,31 @@ EMPTY = 0
 
 class Move:
     def __init__(self, color, coordinate):
-        pass
-
+        self.__value=[color,coordinate[0],coordinate[1]]
+    def get_value(self):
+        return self.__value
 class Board:
     def __init__(self):
         # from constant import BLACK, WHITE, EMPTY
-        self.__board = None # [[EMPTY]*n]*n
-    
+        def constructing_board():
+            board=[]
+            for _ in range(15):
+                tmp=[]
+                for i in range(15):
+                    tmp.append(EMPTY)
+                board.append(tmp)
+            return board
+        self.__board = constructing_board() # [[EMPTY]*n]*n
+        
     def update(self, move):
-        pass
+        move_value=move.get_value()
+        self.__board[move_value[1]][move_value[2]] = move_value[0]
     
     def get_board(self):
         return self.__board
 
 class User:
-    def __init__(self):
+    def __init__(self,socket,addr):
         self.ip = None
+        self.socket = socket
+        self.addr = addr
